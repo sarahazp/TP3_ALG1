9cca7bbc64568fafc9a0818b1b2dc3cf36f281b4
Sarah Azevedo Pereira <sarahComH@proton.me>
2025-02-11 12:27:53 -0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -3,8 +3,6 @@
 #include "brute_force.hpp"
 #include "dinamic.hpp"
 
-#include <chrono>
-
 int main() {
     std::ios::sync_with_stdio(false);
     std::cin.tie(nullptr);
@@ -16,27 +14,19 @@ int main() {
     char tipo = ' ';
     std::cin >> tipo;
     
-   /* switch (tipo)
+    switch (tipo)
     {
     case 'b':
         b.init_Brute();
         break;
-    case 'd':*/
-    	auto start = std::chrono::high_resolution_clock::now();
-        d.init_Dinamico();
-        //b.init_Brute();
-        //g.init_Greedy();
-        auto end = std::chrono::high_resolution_clock::now();
-   /*     break;
     case 'd':
+        d.init_Dinamico();
+        break;
+    case 'g':
         g.init_Greedy();
         break;
     default:
         break;
-    }*/
-    std::chrono::duration<double, std::milli> duration = end - start;
-
-    // Exibe o tempo gasto
-    std::cout << duration.count() <<  std::endl;
+    }
 
 }
